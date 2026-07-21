e9c11c97910bc302db3c75eb80aa127bd99e0689
Eunji Ha <eh0129s@gmail.com>
2026-07-21 17:25:32 +0900

    update readme and pubs

--- assets/js/publications-data.js
+++ assets/js/publications-data.js
@@ -10,139 +10,139 @@
 window.PUBLICATIONS = [
   {
     title: "Reprogramming of cancer metabolism via photoresponsive nano-PROTAC enhances pyroptosis-mediated immunotherapy",
-    url: "#",
+    url: "https://doi.org/10.1038/s41392-025-02405-6",
     authors: "B Park, <b>J Choi</b>, JH Lee, Y Kim, W Lee, A Lee, IC Sun, HY Yoon, Y Kim, \u2026",
     venue: "Signal Transduction and Targeted Therapy", year: "2025"
   },
   {
     title: "Targeted delivery of miR-34a via anti-CD47 antibody conjugates for enhanced cancer immunotherapy in triple negative breast cancer",
-    url: "#",
+    url: "https://doi.org/10.1002/smll.202504468",
     authors: "Y Ryu, EH Kim, H Jang, Y Kim, B Park, <b>J Choi</b>, Y Jang, SG Chi, MK Shim, \u2026",
     venue: "Small", year: "2025"
   },
   {
     title: "Repurposing mesalamine for acute kidney injury through supramolecular assembly",
-    url: "#",
+    url: "https://doi.org/10.1016/j.jconrel.2025.114041",
     authors: "B Park, D Park, H Jang, YS Ko, HY Lee, YE Choi, <b>J Choi</b>, SW Oh, SK Jo, \u2026",
     venue: "Journal of Controlled Release", year: "2025"
   },
   {
     title: "Activatable PROTAC nanoassembly for photodynamic PTP1B proteolysis enhances glioblastoma immunotherapy",
-    url: "#",
+    url: "https://doi.org/10.1016/j.apsb.2025.06.028",
     authors: "Y Jang, <b>J Choi</b>, B Park, JY Park, JH Lee, J Goo, D Shin, SH Kim, Y Kim, \u2026",
     venue: "Acta Pharmaceutica Sinica B", year: "2025"
   },
   {
     title: "Apolipoprotein fusion enables spontaneous functionalization of mRNA lipid nanoparticles with antibody for targeted cancer therapy",
-    url: "#",
+    url: "https://doi.org/10.1021/acsnano.4c16562",
     authors: "W Park, <b>J Choi</b>, J Hwang, S Kim, Y Kim, MK Shim, W Park, S Yu, S Jung, \u2026",
     venue: "ACS Nano", year: "2025"
   },
   {
     title: "Drug repositioning of mesalamine via supramolecular nanoassembly for the treatment of drug-induced acute liver failure",
-    url: "#",
+    url: "https://doi.org/10.7150/thno.101358",
     authors: "B Park, EH Kim, H Jang, Y Kim, Y Ryu, <b>J Choi</b>, D Shin, MC Lee, Y Yang, \u2026",
     venue: "Theranostics", year: "2025"
   },
   {
     title: "Stiffness-tunable velvet worm-inspired soft adhesive robot",
-    url: "#",
+    url: "https://doi.org/10.1126/sciadv.adp8260",
     authors: "H Min, D Bae, S Jang, S Lee, M Park, CB Dayan, <b>J Choi</b>, K Bak, Y Yang, \u2026",
     venue: "Science Advances", year: "2024"
   },
   {
     title: "Light-triggered PROTAC nanoassemblies for photodynamic IDO proteolysis in cancer immunotherapy",
-    url: "#",
+    url: "https://doi.org/10.1002/adma.202405475",
     authors: "<b>J Choi</b>, B Park, JY Park, D Shin, S Lee, HY Yoon, K Kim, SH Kim, Y Kim, \u2026",
     venue: "Advanced Materials", year: "2024"
   },
   {
     title: "Milk-derived extracellular vesicles enable gut-to-tumor oral delivery of tumor-activated doxorubicin prodrugs",
-    url: "#",
+    url: "https://doi.org/10.7150/thno.97269",
     authors: "H Jang, <b>J Choi</b>, D Park, G Han, EH Kim, K Kim, SH Kim, MK Shim, Y Yang",
     venue: "Theranostics", year: "2024"
   },
   {
     title: "Implantable MicroLED-mediated chemo-photodynamic combination therapy for glioma treatment",
-    url: "#",
+    url: "https://doi.org/10.1002/adfm.202316386",
     authors: "J Kim, HK Lee, J Park, SI Jeon, IS Lee, WS Yun, Y Moon, <b>J Choi</b>, MK Shim, \u2026",
     venue: "Advanced Functional Materials", year: "2024"
   },
   {
     title: "Targeted delivery of anti-miRNA21 sensitizes PD-L1high tumor to immunotherapy by promoting immunogenic cell death",
-    url: "#",
+    url: "https://doi.org/10.7150/thno.97755",
     authors: "EH Kim, <b>J Choi</b>, H Jang, Y Kim, JW Lee, Y Ryu, J Choi, Y Choi, SG Chi, \u2026",
     venue: "Theranostics", year: "2024"
   },
   {
     title: "Design of PD-L1-targeted lipid nanoparticles to turn on PTEN for efficient cancer therapy",
-    url: "#",
+    url: "https://doi.org/10.1002/advs.202309917",
     authors: "Y Kim, <b>J Choi</b>, EH Kim, W Park, H Jang, Y Jang, SG Chi, DH Kweon, \u2026",
     venue: "Advanced Science", year: "2024"
   },
   {
     title: "Thermally managed, injectable optoelectronic probe with heat dissipation guide for photodynamic therapy",
-    url: "#",
+    url: "https://doi.org/10.1002/smll.202300753",
     authors: "JS Lee, <b>J Choi</b>, GY Park, SJ Kang, J Yang, Y Lee, M Choi, K Kim, T Kim",
     venue: "Small", year: "2023"
   },
   {
     title: "Immune checkpoint-targeted drug conjugates: A promising tool for remodeling tumor immune microenvironment",
-    url: "#",
+    url: "https://doi.org/10.1016/j.jconrel.2023.05.031",
     authors: "<b>J Choi</b>, H Jang, <b>J Choi</b>, Y Choi, Y Yang, MK Shim, SH Kim",
     venue: "Journal of Controlled Release", year: "2023"
   },
   {
     title: "Liposome-mediated PD-L1 multivalent binding promotes the lysosomal degradation of PD-L1 for T cell-mediated antitumor immunity",
-    url: "#",
+    url: "https://doi.org/10.1016/j.biomaterials.2022.121841",
     authors: "S Yang, MK Shim, S Song, H Cho, <b>J Choi</b>, SI Jeon, WJ Kim, W Um, \u2026",
     venue: "Biomaterials", year: "2022"
   },
   {
     title: "Comparative study of cathepsin B-cleavable linkers for the optimal design of cathepsin B-specific doxorubicin prodrug nanoparticles for targeted cancer therapy",
-    url: "#",
+    url: "https://doi.org/10.1016/j.biomaterials.2022.121806",
     authors: "N Shim, SI Jeon, S Yang, JY Park, M Jo, J Kim, <b>J Choi</b>, WS Yun, J Kim, \u2026",
     venue: "Biomaterials", year: "2022"
   },
   {
     title: "Light-triggered photodynamic nanomedicines for overcoming localized therapeutic efficacy in cancer treatment",
-    url: "#",
+    url: "https://doi.org/10.1016/j.addr.2022.114344",
     authors: "<b>J Choi</b>, IC Sun, HS Hwang, HY Yoon, K Kim",
     venue: "Advanced Drug Delivery Reviews", year: "2022"
   },
   {
     title: "PDL1-binding peptide/anti-miRNA21 conjugate as a therapeutic modality for PD-L1high tumors and TAMs",
-    url: "#",
+    url: "https://doi.org/10.1016/j.jconrel.2022.02.031",
     authors: "EH Kim, J Lee, G Kwak, H Jang, H Kim, H Cho, Y Jang, <b>J Choi</b>, SG Chi, \u2026",
     venue: "Journal of Controlled Release", year: "2022"
   },
   {
     title: "Anti-PD-L1 peptide-conjugated prodrug nanoparticles for targeted cancer immunotherapy combining PD-L1 blockade with immunogenic cell death",
-    url: "#",
+    url: "https://doi.org/10.7150/thno.69119",
     authors: "Y Moon, MK Shim, <b>J Choi</b>, S Yang, J Kim, WS Yun, H Cho, JY Park, Y Kim, \u2026",
     venue: "Theranostics", year: "2022"
   },
   {
     title: "The safe and effective intraperitoneal chemotherapy with cathepsin B-specific doxorubicin prodrug nanoparticles in ovarian cancer with peritoneal carcinomatosis",
-    url: "#",
+    url: "https://doi.org/10.1016/j.biomaterials.2021.121189",
     authors: "J Kim, MK Shim, YJ Cho, S Jeon, Y Moon, <b>J Choi</b>, J Kim, J Lee, JW Lee, \u2026",
     venue: "Biomaterials", year: "2021"
   },
   {
     title: "Visible-light-triggered prodrug nanoparticles combine chemotherapy and photodynamic therapy to potentiate checkpoint blockade cancer immunotherapy",
-    url: "#",
+    url: "https://doi.org/10.1021/acsnano.1c03416",
     authors: "<b>J Choi</b>, MK Shim, S Yang, HS Hwang, H Cho, J Kim, WS Yun, Y Moon, \u2026",
     venue: "ACS Nano", year: "2021"
   },
   {
     title: "Cancer-activated doxorubicin prodrug nanoparticles induce preferential immune response with minimal doxorubicin-related toxicity",
-    url: "#",
+    url: "https://doi.org/10.1016/j.biomaterials.2021.120791",
     authors: "S Yang, MK Shim, WJ Kim, <b>J Choi</b>, GH Nam, J Kim, J Kim, Y Moon, \u2026",
     venue: "Biomaterials", year: "2021"
   },
   {
     title: "Combination of cancer-specific prodrug nanoparticle with Bcl-2 inhibitor to overcome acquired drug resistance",
-    url: "#",
+    url: "https://doi.org/10.1016/j.jconrel.2020.10.065",
     authors: "J Kim, MK Shim, S Yang, Y Moon, S Song, <b>J Choi</b>, J Kim, K Kim",
     venue: "Journal of Controlled Release", year: "2021"
   }
